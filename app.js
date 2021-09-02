b1dc8787d37b86311873da59f83f8b92c4516af7
rahul31agrawal <rahul31agrawal@gmail.com>
2021-09-02 21:26:24 +0530

    input-output-processing-done

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked")
-})
+
+function clickHandler(){
+    outputDiv.innerText="sdasfafa "+ txtInput.value;
+
+}
+
+
+btnTranslate.addEventListener("click", clickHandler)
